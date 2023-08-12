3a81a6837ecd03e4e93756e4c39dc7ace5d238c2
Paulo <106405308+Paulo19961944@users.noreply.github.com>
2023-08-12 16:11:41 -0300

    Update Switch.java

--- src/Switch.java
+++ src/Switch.java
@@ -1,7 +1,7 @@
 public class Switch {
     public static void main(String[] args) {
         /** Variáveis */
-        String[] dias = {"domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"};
+        String[] dias = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 
         /** Números de 0 a 6 */
         int[] numeros = {0, 1, 2, 3, 4, 5, 6};
@@ -10,25 +10,32 @@ public class Switch {
         for (int i = 0; i < numeros.length; i++) {
             switch (numeros[i]) {
                 case 0:
-                    System.out.println("Dia " + numeros[i] + ": " + dias[i]);
+                    dias[0] = "Domingo";
+                    System.out.println("Dia " +": " + dias[0]);
                     break;
                 case 1:
-                    System.out.println("Dia " + numeros[i] + ": " + dias[i]);
+                    dias[1] = "Segunda-Feira";
+                    System.out.println("Dia " + ": " + dias[1]);
                     break;
                 case 2:
-                    System.out.println("Dia " + numeros[i] + ": " + dias[i]);
+                    dias[2] = "Terca-Feira";
+                    System.out.println("Dia " + ": " + dias[2]);
                     break;
                 case 3:
-                    System.out.println("Dia " + numeros[i] + ": " + dias[i]);
+                    dias[3] = "Quarta-Feira";
+                    System.out.println("Dia " + ": " + dias[3]);
                     break;
                 case 4:
-                    System.out.println("Dia " + numeros[i] + ": " + dias[i]);
+                    dias[4] = "Quinta-Feira";
+                    System.out.println("Dia " + ": " + dias[4]);
                     break;
                 case 5:
-                    System.out.println("Dia " + numeros[i] + ": " + dias[i]);
+                    dias[5] = "Sexta-Feira";
+                    System.out.println("Dia "+ ": " + dias[5]);
                     break;
                 case 6:
-                    System.out.println("Dia " + numeros[i] + ": " + dias[i]);
+                    dias[6] = "Sabado";
+                    System.out.println("Dia " + ": " + dias[6]);
                     break;
                 default:
                     System.out.println("Dia inválido");
